77622d3a53c279f01a842b007b3c07cf9629e8d3
firecamp <twh415@gmail.com>
2014-02-12 22:56:04 -0500

    minor changes

--- src/ca/xef6/firecamp/profile/ProfileFragment.java
+++ src/ca/xef6/firecamp/profile/ProfileFragment.java
@@ -12,20 +12,22 @@ import ca.xef6.firecamp.R;
 
 public class ProfileFragment extends Fragment {
 
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-	inflater.inflate(R.menu.fragment_profile, menu);
-    }
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.fragment_profile, menu);
+	}
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-	View view = inflater.inflate(R.layout.fragment_profile, container, false);
-	return view;
-    }
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_profile, container,
+				false);
+		return view;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-	return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return super.onOptionsItemSelected(item);
+	}
 
 }
--- src/ca/xef6/firecamp/profile/SessionLoginFragment.java
+++ src/ca/xef6/firecamp/profile/SessionLoginFragment.java
@@ -31,6 +31,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import ca.xef6.firecamp.R;
 
@@ -42,120 +43,135 @@ import com.facebook.widget.LoginButton;
 import com.facebook.widget.ProfilePictureView;
 
 public class SessionLoginFragment extends Fragment {
-    private static final String URL_PREFIX_FRIENDS = "https://graph.facebook.com/me/friends?access_token=";
+	LinearLayout profilelayout;
 
-    private TextView textInstructionsOrLink;
-    private LoginButton buttonLoginLogout;
-    private ProfilePictureView profilePictureView;
-    private Session.StatusCallback statusCallback = new SessionStatusCallback();
+	private static final String URL_PREFIX_FRIENDS = "https://graph.facebook.com/me/friends?access_token=";
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-	View view = inflater.inflate(R.layout.fragment, container, false);
+	private TextView textInstructionsOrLink;
+	private LoginButton buttonLoginLogout;
+	private ProfilePictureView profilePictureView;
+	private Session.StatusCallback statusCallback = new SessionStatusCallback();
 
-	buttonLoginLogout = (LoginButton) view.findViewById(R.id.buttonLoginLogout);
-	textInstructionsOrLink = (TextView) view.findViewById(R.id.instructionsOrLink);
-	profilePictureView = (ProfilePictureView) view.findViewById(R.id.profilepic);
-
-	Settings.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
-
-	Session session = Session.getActiveSession();
-	if (session == null) {
-	    if (savedInstanceState != null) {
-		session = Session.restoreSession(getActivity(), null, statusCallback, savedInstanceState);
-	    }
-	    if (session == null) {
-		session = new Session(getActivity());
-	    }
-	    Session.setActiveSession(session);
-	    if (session.getState().equals(SessionState.CREATED_TOKEN_LOADED)) {
-		session.openForRead(new Session.OpenRequest(this).setCallback(statusCallback));
-	    }
-	}
-
-	updateView(session);
-	try {
-	    PackageInfo info = getActivity().getPackageManager().getPackageInfo("ca.xef6.firecamp", PackageManager.GET_SIGNATURES);
-	    for (Signature signature : info.signatures) {
-		MessageDigest md = MessageDigest.getInstance("SHA");
-		md.update(signature.toByteArray());
-		Log.w("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-	    }
-	} catch (NameNotFoundException e) {
-
-	} catch (NoSuchAlgorithmException e) {
-
-	}
-	return view;
-    }
-
-    @Override
-    public void onStart() {
-	super.onStart();
-	Session.getActiveSession().addCallback(statusCallback);
-    }
-
-    @Override
-    public void onStop() {
-	super.onStop();
-	Session.getActiveSession().removeCallback(statusCallback);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-	super.onActivityResult(requestCode, resultCode, data);
-	Session.getActiveSession().onActivityResult(getActivity(), requestCode, resultCode, data);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-	super.onSaveInstanceState(outState);
-	Session session = Session.getActiveSession();
-	Session.saveSession(session, outState);
-    }
-
-    private void updateView(Session session) {
-	// Session session = Session.getActiveSession();
-	if (session.isOpened()) {
-
-	    textInstructionsOrLink.setText(URL_PREFIX_FRIENDS + session.getAccessToken());
-	    buttonLoginLogout.setText("Logout");
-	    buttonLoginLogout.setOnClickListener(new View.OnClickListener() {
-		public void onClick(View view) {
-		    onClickLogout();
-		}
-	    });
-	} else {
-	    textInstructionsOrLink.setText("instructions");
-	    buttonLoginLogout.setText("login");
-	    buttonLoginLogout.setOnClickListener(new View.OnClickListener() {
-		public void onClick(View view) {
-		    onClickLogin();
-		}
-	    });
-	}
-    }
-
-    private void onClickLogin() {
-	Session session = Session.getActiveSession();
-	if (!session.isOpened() && !session.isClosed()) {
-	    session.openForRead(new Session.OpenRequest(this).setCallback(statusCallback));
-	} else {
-	    Session.openActiveSession(getActivity(), this, true, statusCallback);
-	}
-    }
-
-    private void onClickLogout() {
-	Session session = Session.getActiveSession();
-	if (!session.isClosed()) {
-	    session.closeAndClearTokenInformation();
-	}
-    }
-
-    private class SessionStatusCallback implements Session.StatusCallback {
 	@Override
-	public void call(Session session, SessionState state, Exception exception) {
-	    updateView(session);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment, container, false);
+
+		buttonLoginLogout = (LoginButton) view
+				.findViewById(R.id.buttonLoginLogout);
+		textInstructionsOrLink = (TextView) view
+				.findViewById(R.id.instructionsOrLink);
+		profilePictureView = (ProfilePictureView) view
+				.findViewById(R.id.profilepic);
+		profilelayout = (LinearLayout) view.findViewById(R.id.profile_layout);
+
+		Settings.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
+
+		Session session = Session.getActiveSession();
+		if (session == null) {
+			if (savedInstanceState != null) {
+				session = Session.restoreSession(getActivity(), null,
+						statusCallback, savedInstanceState);
+			}
+			if (session == null) {
+				session = new Session(getActivity());
+			}
+			Session.setActiveSession(session);
+			if (session.getState().equals(SessionState.CREATED_TOKEN_LOADED)) {
+				session.openForRead(new Session.OpenRequest(this)
+						.setCallback(statusCallback));
+			}
+		}
+
+		updateView(session);
+		try {
+			PackageInfo info = getActivity().getPackageManager()
+					.getPackageInfo("ca.xef6.firecamp",
+							PackageManager.GET_SIGNATURES);
+			for (Signature signature : info.signatures) {
+				MessageDigest md = MessageDigest.getInstance("SHA");
+				md.update(signature.toByteArray());
+				Log.w("KeyHash:",
+						Base64.encodeToString(md.digest(), Base64.DEFAULT));
+			}
+		} catch (NameNotFoundException e) {
+
+		} catch (NoSuchAlgorithmException e) {
+
+		}
+		return view;
 	}
-    }
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		Session.getActiveSession().addCallback(statusCallback);
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		Session.getActiveSession().removeCallback(statusCallback);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Session.getActiveSession().onActivityResult(getActivity(), requestCode,
+				resultCode, data);
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		Session session = Session.getActiveSession();
+		Session.saveSession(session, outState);
+	}
+
+	private void updateView(Session session) {
+		// Session session = Session.getActiveSession();
+		if (session.isOpened()) {
+			textInstructionsOrLink.setText(URL_PREFIX_FRIENDS
+					+ session.getAccessToken());
+			buttonLoginLogout.setText("Logout");
+			buttonLoginLogout.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View view) {
+					onClickLogout();
+				}
+			});
+		} else {
+			textInstructionsOrLink.setText("instructions");
+			buttonLoginLogout.setText("login");
+			buttonLoginLogout.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View view) {
+					onClickLogin();
+				}
+			});
+		}
+	}
+
+	private void onClickLogin() {
+		Session session = Session.getActiveSession();
+		if (!session.isOpened() && !session.isClosed()) {
+			session.openForRead(new Session.OpenRequest(this)
+					.setCallback(statusCallback));
+		} else {
+			Session.openActiveSession(getActivity(), this, true, statusCallback);
+		}
+	}
+
+	private void onClickLogout() {
+		Session session = Session.getActiveSession();
+		if (!session.isClosed()) {
+			session.closeAndClearTokenInformation();
+		}
+	}
+
+	private class SessionStatusCallback implements Session.StatusCallback {
+		@Override
+		public void call(Session session, SessionState state,
+				Exception exception) {
+			updateView(session);
+		}
+	}
 }
