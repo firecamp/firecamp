c6c2b67ca230a6bad0e750a6abe4907a58ffcaa5
FS <u1m6xq2r@gmail.com>
2014-02-17 15:53:26 -0500

    Profile page now displays picture and name.

--- src/ca/xef6/firecamp/profile/ProfileFragment.java
+++ src/ca/xef6/firecamp/profile/ProfileFragment.java
@@ -1,33 +1,116 @@
 package ca.xef6.firecamp.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import ca.xef6.firecamp.R;
+
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.ProfilePictureView;
 
 public class ProfileFragment extends Fragment {
 
-	@Override
-	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-		inflater.inflate(R.menu.fragment_profile, menu);
-	}
+    ProfilePictureView        profilePictureView;
+    TextView                  userNameView;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_profile, container,
-				false);
-		return view;
-	}
+    private UiLifecycleHelper uiLifecycleHelper;
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_profile, menu);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        profilePictureView = (ProfilePictureView) view.findViewById(R.id.profile_picture_view);
+        userNameView = (TextView) view.findViewById(R.id.user_name_view);
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiLifecycleHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        uiLifecycleHelper = new UiLifecycleHelper(getActivity(), new Session.StatusCallback() {
+
+            @Override
+            public void call(Session session, SessionState state, Exception exception) {
+                onSessionStateChange(session, state, exception);
+            }
+
+        });
+        uiLifecycleHelper.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiLifecycleHelper.onDestroy();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiLifecycleHelper.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        uiLifecycleHelper.onResume();
+        setUserData(Session.getActiveSession());
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiLifecycleHelper.onSaveInstanceState(outState);
+    }
+
+    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        setUserData(session);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        uiLifecycleHelper.onStop();
+    }
+
+    private void setUserData(final Session session) {
+        Request request = Request.newMeRequest(session, new Request.GraphUserCallback() {
+            @Override
+            public void onCompleted(GraphUser user, Response response) {
+                if (session == Session.getActiveSession()) {
+                    if (user != null) {
+                        profilePictureView.setProfileId(user.getId());
+                        userNameView.setText(user.getName());
+                    }
+                }
+                if (response.getError() != null) {
+                    //handleError(response.getError());
+                }
+            }
+        });
+        request.executeAsync();
+
+    }
 
 }
